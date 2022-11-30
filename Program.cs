f189dbdb4c6b4947f1f60e42aa3becdb7d8c1f35
Evgeniy <freemen0083@gmail.com>
2022-11-30 22:07:24 +0400

    Prog№1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Enter number1 ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number2 ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+if (numb1 > numb2)
+{
+    Console.Write("Max number is " + numb1);
+    Console.Write("     Min number is: " + numb2);
+}
+else
+{
+    Console.Write("Max number is " + numb2);
+    Console.Write("     Min number is: " + numb1);
+}
