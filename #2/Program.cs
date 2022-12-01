0de284fdc0f9e25f4ead260d02fe04a0d7040b74
Evgeniy <freemen0083@gmail.com>
2022-12-01 18:37:35 +0400

    Prog№2

---
+++ #2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter number1  ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number2  ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number3  ");
+int numb3 = Convert.ToInt32(Console.ReadLine());
+int max=numb1;
+if (numb2 > max)
+{
+    max = numb2;
+}
+else
+{
+    numb1 = max;
+}
+
+if (numb3 > max)
+{
+    max = numb3;
+}
+else
+{
+    numb1 = max;
+}
+
+Console.Write("Max number is " + max);
