0e1311adc2d6948a68a0311752f149d2c5564e10
Evgeniy <freemen0083@gmail.com>
2022-12-01 20:00:18 +0400

    Prog№3

---
+++ #3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number:");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb%2==0)
+{
+    Console.WriteLine(numb+"  is even number:");
+}
+else
+{
+    Console.WriteLine(numb+ "  is not even number");
+}
